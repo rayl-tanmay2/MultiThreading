a383ca7160892a87b5859d9024c23ce830607052
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-04-19 19:23:38 +0530

    Created instances of each thread

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
-
+// Name - Tanmay Joshi
+ // PRN: 23070126134
+ // Batch: AIML B3
+ 
+ // Main.java
+ 
+public class Main {
+     public static void main(String[] args) {
+         // Creating instances of each thread class
+         EvenThread evenThread = new EvenThread();
+         OddThread oddThread = new OddThread();
+         CharThread charThread = new CharThread();
+ 
+ }
+   
