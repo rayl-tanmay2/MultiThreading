52d08f71d68b309228e6081ce3e28b1093257b35
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-04-19 19:24:30 +0530

    Inititated all threads

--- Main.java
+++ Main.java
@@ -10,6 +10,12 @@ public class Main {
          EvenThread evenThread = new EvenThread();
          OddThread oddThread = new OddThread();
          CharThread charThread = new CharThread();
+
+      // Starting each thread
+         evenThread.start();
+         oddThread.start();
+         charThread.start();
+     }
  
  }
    
